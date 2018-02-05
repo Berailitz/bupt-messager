74b860d93875d429ac9267dc9c8c0df58a38cce8
berailitz <admin@ohhere.xyz>
2018-02-05 18:22:31 +0800

    Backend -> BotBackend BotBackend: fix int/str in latest_command, add logging for new chat

--- bupt_messager/bot_handle/bot_backend.py
+++ bupt_messager/bot_handle/bot_backend.py
@@ -1,7 +1,8 @@
+import logging
 from telegram import ParseMode
 from ..config import BOT_NOTICE_LIST_LENGTH
 
-class Backend(object):
+class BotBackend(object):
     def __init__(self, sql_handle=None):
         self.sql_handle = sql_handle
 
@@ -10,10 +11,17 @@ class Backend(object):
 
     def start_command(self, bot, update):
         bot.send_message(chat_id=update.message.chat_id, text="Welcome.")
-        self.sql_handle.insert_chat(update.message.chat_id)
+        insert_result = self.sql_handle.insert_chat(update.message.chat_id)
+        if insert_result is not None:
+            logging.info(f'BotBackend.start_command: new chat `{insert_result.id}`.')
 
     def latest_command(self, bot, update, args):
-        length = args[0] if args else BOT_NOTICE_LIST_LENGTH
+        try:
+            length = int(args[0]) if args else BOT_NOTICE_LIST_LENGTH
+        except ValueError as identifier:
+            bot.send_message(chat_id=update.message.chat_id, text="Didn't understand...", parse_mode=ParseMode.MARKDOWN)
+            logging.info(f'BotBackend.latest_command: {identifier}')
+            return
         text = ""
         for index, notice in enumerate(self.sql_handle.get_latest_notices(length)):
             text += f'{index + 1}.[{notice.title}]({notice.url})\n'
--- bupt_messager/bot_handle/bot_handle.py
+++ bupt_messager/bot_handle/bot_handle.py
@@ -1,26 +1,26 @@
 import logging
 from telegram.ext import Updater, CommandHandler
 from ..sql_handle import SQLHandle
-from .backend import Backend
+from .bot_backend import BotBackend
 
 class BotHandle(object):
     def __init__(self, sql_manager=None, bot=None):
-        self.backend = Backend(sql_handle=SQLHandle(sql_manager=sql_manager))
+        self.bot_backend = BotBackend(sql_handle=SQLHandle(sql_manager=sql_manager))
         self.updater = Updater(bot=bot) if bot else None
 
-    def init_backend(self, sql_manager):
-        self.backend.sql_handle.init_sql_manager(sql_manager)
+    def init_bot_backend(self, sql_manager):
+        self.bot_backend.sql_handle.init_sql_manager(sql_manager)
 
     def init_updater(self, bot):
         self.updater = Updater(bot=bot)
 
     def add_handler(self):
         dispatcher = self.updater.dispatcher
-        start_handler = CommandHandler('start', self.backend.start_command)
+        start_handler = CommandHandler('start', self.bot_backend.start_command)
         dispatcher.add_handler(start_handler)
-        latest_handler = CommandHandler('latest', self.backend.latest_command, pass_args=True)
+        latest_handler = CommandHandler('latest', self.bot_backend.latest_command, pass_args=True)
         dispatcher.add_handler(latest_handler)
-        yo_handler = CommandHandler('yo', self.backend.yo_command)
+        yo_handler = CommandHandler('yo', self.bot_backend.yo_command)
         dispatcher.add_handler(yo_handler)
 
     def start(self):

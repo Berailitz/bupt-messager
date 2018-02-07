76614814fb0903b1465e9b1c6284fd503d94b3e0
berailitz <admin@ohhere.xyz>
2018-02-07 17:00:36 +0800

    BotHandler: add_handler: fix rags for read_handler

--- bupt_messager/bot_handler/bot_handler.py
+++ bupt_messager/bot_handler/bot_handler.py
@@ -23,7 +23,7 @@ class BotHandler(object):
         dispatcher.add_handler(latest_handler)
         status_handler = CommandHandler('status', self.bot_backend.status_command, pass_args=True)
         dispatcher.add_handler(status_handler)
-        read_handler = CommandHandler('read', self.bot_backend.read_command)
+        read_handler = CommandHandler('read', self.bot_backend.read_command, pass_args=True)
         dispatcher.add_handler(read_handler)
         yo_handler = CommandHandler('yo', self.bot_backend.yo_command)
         dispatcher.add_handler(yo_handler)
